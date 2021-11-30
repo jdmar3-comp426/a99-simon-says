345157bce3c1c519683e5de510d17900e00c0611
Maya Agnihotri <mayaagni@live.unc.edu>
2021-11-30 12:23:26 -0500

    work on js connect pt 2

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 let sequence = [];
 let humanSequence = [];
+let gameLevel = 0;
 
 const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
 
+function activateTile(color) {
+    const sqColor = document.querySelector(`[data-type='${color}']`);  
+    sqColor.classList.add('activated');
+    setTimeout(() => {
+        tile.classList.remove('activated');
+      }, 300);
+}
+
+function playRound(nextSequence) {
+    nextSequence.forEach((color, index) => {
+        setTimeout(() => {
+            activateTile(color);
+        }, (index + 1) * 600);
+    });
+}
+
+function nextStep() {
+    const tiles = ['red', 'yellow', 'green', 'blue'];
+    const random = tiles[Math.floor(Math.random() * tiles.length)];
+    return random;
+}
+
+function nextRound() {
+    gameLevel += 1;
+    const nextSequence = [...sequence];
+    nextSequence.push(nextStep());
+    playRound(nextSequence);
+}
+
 function startGame() {
-  startButton.classList.add('hidden');
-  info.classList.remove('hidden');
-  info.textContent = 'Ready to play!';
+    startButton.classList.add('hidden');
+    info.classList.remove('hidden');
+    info.textContent = 'Ready to play!';
+    nextRound();
 }
 
 startButton.addEventListener('click', startGame);
