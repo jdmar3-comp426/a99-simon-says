984669db122f8f423b59da0ccebb4270809832f8
ellenhayes <69438924+ellenhayes@users.noreply.github.com>
2021-12-01 03:41:34 -0500

    simon game working

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let sequence = [];
 let humanSequence = [];
 let gameLevel = 0;
+// get highscore
+let highScore = 0;
+
+const heading = document.querySelector('.js-heading');
+const tileContainer = document.querySelector('.js-container');
+
 
 const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
@@ -14,9 +20,7 @@ function activateTile(color) {
 }
 
 function playRound(nextSequence) {
-    if(gameLevel > 1) {
-        info.classList.add('hidden');
-    }
+   
     nextSequence.forEach((color, index) => {
         setTimeout(() => {
             activateTile(color);
@@ -32,9 +36,18 @@ function nextStep() {
 
 function nextRound() {
     gameLevel += 1;
+    tileContainer.classList.add('unclickable');
+    info.textContent = 'Wait for the computer';
+    heading.textContent = `Level ${gameLevel} of 20`
+   
+
     const nextSequence = [...sequence];
     nextSequence.push(nextStep());
     playRound(nextSequence);
+    sequence = [...nextSequence];
+    setTimeout(() => {
+        humanTurn(gameLevel);
+    }, gameLevel * 600 + 1000);
 }
 
 function startGame() {
@@ -44,4 +57,85 @@ function startGame() {
     nextRound();
 }
 
+function humanTurn(gameLevel) {
+    tileContainer.classList.remove('unclickable');
+    info.textContent = `Your turn: ${gameLevel} Tap${gameLevel > 1 ? 's' : ''}`;
+}
+
+tileContainer.addEventListener('click', event => {
+    const sqColor = event.target.dataset;
+    console.log(sqColor.type);
+    
+    if (sqColor) handleClick(sqColor.type);
+});
+
+
+ function handleClick(tile) {
+    info.textContent = `tile clicked`;
+    const index = humanSequence.push(tile) - 1;
+  
+    const remainingTaps = sequence.length - humanSequence.length;
+  
+    if (humanSequence[index] !== sequence[index]) {
+        resetGame('Oops! Game over, you pressed the wrong tile');
+        return;
+      }
+
+    if (humanSequence.length === sequence.length) {
+      humanSequence = [];
+      info.textContent = 'Success! Keep going!';
+      setTimeout(() => {
+        nextRound();
+      }, 1000);
+      score();
+      return;
+    }
+  
+    if (humanSequence.length === sequence.length) {
+        if (humanSequence.length === 20) {
+          resetGame('Congrats! You completed all the levels');
+          return
+        }
+
+        humanSequence = [];
+        info.textContent = 'Success! Keep going!';
+        setTimeout(() => {
+          nextRound();
+        }, 1000);
+        score();
+        return;
+      }
+
+    info.textContent = `Your turn: ${remainingTaps} Tap${
+      remainingTaps > 1 ? 's' : ''
+    }`;
+
+  }
+
+  function resetGame(text) {
+    alert(text);
+    sequence = [];
+    humanSequence = [];
+    gameLevel = 0;
+    startButton.classList.remove('hidden');
+    heading.textContent = 'Simon Game';
+    info.classList.add('hidden');
+    tileContainer.classList.add('unclickable');
+    // if (gameLevel > GET HIGH SCORE)
+    //      New div that says new high score
+    // set high score send back to form
+    if (gameLevel > highScore) {
+        sethighScore();
+    }
+  }
+
+  function score() {
+    document.getElementById("score").innerHTML = gameLevel;
+    console.log(gameLevel);
+    }
+    
+    function sethighScore() {
+        // new html content
+        highScore.classList.remove('hidden');
+        }
 startButton.addEventListener('click', startGame);
