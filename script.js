23ed3226425fe7419e26f24bc8e085acc3081d81
Maya Agnihotri <mayaagni@live.unc.edu>
2021-12-01 15:16:12 -0500

    finished front end

--- script.js
+++ script.js
@@ -12,130 +12,130 @@ const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
 
 function activateTile(color) {
-    const sqColor = document.querySelector(`[data-type='${color}']`);  
-    sqColor.classList.add('activated');
-    setTimeout(() => {
-        sqColor.classList.remove('activated');
-      }, 300);
+  const sqColor = document.querySelector(`[data-type='${color}']`);
+  sqColor.classList.add('activated');
+  setTimeout(() => {
+    sqColor.classList.remove('activated');
+  }, 300);
 }
 
 function playRound(nextSequence) {
-   
-    nextSequence.forEach((color, index) => {
-        setTimeout(() => {
-            activateTile(color);
-        }, (index + 1) * 600);
-    });
+
+  nextSequence.forEach((color, index) => {
+    setTimeout(() => {
+      activateTile(color);
+    }, (index + 1) * 600);
+  });
 }
 
 function nextStep() {
-    const tiles = ['red', 'yellow', 'green', 'blue'];
-    const random = tiles[Math.floor(Math.random() * tiles.length)];
-    return random;
+  const tiles = ['red', 'yellow', 'green', 'blue'];
+  const random = tiles[Math.floor(Math.random() * tiles.length)];
+  return random;
 }
 
 function nextRound() {
-    gameLevel += 1;
-    tileContainer.classList.add('unclickable');
-    info.textContent = 'Wait for the computer';
-    heading.textContent = `Level ${gameLevel} of 20`
-   
+  gameLevel += 1;
+  tileContainer.classList.add('unclickable');
+  info.textContent = 'Wait for the computer';
+  heading.textContent = `Level ${gameLevel} of 20`
 
-    const nextSequence = [...sequence];
-    nextSequence.push(nextStep());
-    playRound(nextSequence);
-    sequence = [...nextSequence];
-    setTimeout(() => {
-        humanTurn(gameLevel);
-    }, gameLevel * 600 + 1000);
+
+  const nextSequence = [...sequence];
+  nextSequence.push(nextStep());
+  playRound(nextSequence);
+  sequence = [...nextSequence];
+  setTimeout(() => {
+    humanTurn(gameLevel);
+  }, gameLevel * 600 + 1000);
 }
 
 function startGame() {
-    startButton.classList.add('hidden');
-    info.classList.remove('hidden');
-    info.textContent = 'Ready to play!';
-    nextRound();
+  startButton.classList.add('hidden');
+  info.classList.remove('hidden');
+  info.textContent = 'Ready to play!';
+  nextRound();
 }
 
 function humanTurn(gameLevel) {
-    tileContainer.classList.remove('unclickable');
-    info.textContent = `Your turn: ${gameLevel} Tap${gameLevel > 1 ? 's' : ''}`;
+  tileContainer.classList.remove('unclickable');
+  info.textContent = `Your turn: ${gameLevel} Tap${gameLevel > 1 ? 's' : ''}`;
 }
 
 tileContainer.addEventListener('click', event => {
-    const sqColor = event.target.dataset;
-    console.log(sqColor.type);
-    
-    if (sqColor) handleClick(sqColor.type);
+  const sqColor = event.target.dataset;
+  console.log(sqColor.type);
+
+  if (sqColor) handleClick(sqColor.type);
 });
 
 
- function handleClick(tile) {
-    info.textContent = `tile clicked`;
-    const index = humanSequence.push(tile) - 1;
-  
-    const remainingTaps = sequence.length - humanSequence.length;
-  
-    if (humanSequence[index] !== sequence[index]) {
-        resetGame('Oops! Game over, you pressed the wrong tile');
-        return;
-      }
+function handleClick(tile) {
+  info.textContent = `tile clicked`;
+  const index = humanSequence.push(tile) - 1;
 
-    if (humanSequence.length === sequence.length) {
-      humanSequence = [];
-      info.textContent = 'Success! Keep going!';
-      setTimeout(() => {
-        nextRound();
-      }, 1000);
-      score();
-      return;
+  const remainingTaps = sequence.length - humanSequence.length;
+
+  if (humanSequence[index] !== sequence[index]) {
+    resetGame('Oops! Game over, you pressed the wrong tile');
+    return;
+  }
+
+  if (humanSequence.length === sequence.length) {
+    humanSequence = [];
+    info.textContent = 'Success! Keep going!';
+    setTimeout(() => {
+      nextRound();
+    }, 1000);
+    score();
+    return;
+  }
+
+  if (humanSequence.length === sequence.length) {
+    if (humanSequence.length === 20) {
+      resetGame('Congrats! You completed all the levels');
+      return
     }
-  
-    if (humanSequence.length === sequence.length) {
-        if (humanSequence.length === 20) {
-          resetGame('Congrats! You completed all the levels');
-          return
-        }
 
-        humanSequence = [];
-        info.textContent = 'Success! Keep going!';
-        setTimeout(() => {
-          nextRound();
-        }, 1000);
-        score();
-        return;
-      }
+    humanSequence = [];
+    info.textContent = 'Success! Keep going!';
+    setTimeout(() => {
+      nextRound();
+    }, 1000);
+    score();
+    return;
+  }
 
-    info.textContent = `Your turn: ${remainingTaps} Tap${
-      remainingTaps > 1 ? 's' : ''
+  info.textContent = `Your turn: ${remainingTaps} Tap${remainingTaps > 1 ? 's' : ''
     }`;
 
-  }
+}
 
-  function resetGame(text) {
-    alert(text);
-    sequence = [];
-    humanSequence = [];
-    gameLevel = 0;
-    startButton.classList.remove('hidden');
-    heading.textContent = 'Simon Game';
-    info.classList.add('hidden');
-    tileContainer.classList.add('unclickable');
-    // if (gameLevel > GET HIGH SCORE)
-    //      New div that says new high score
-    // set high score send back to form
-    if (gameLevel > highScore) {
-        sethighScore();
-    }
+function resetGame(text) {
+  alert(text);
+  sequence = [];
+  humanSequence = [];
+  gameLevel = 0;
+  startButton.classList.remove('hidden');
+  heading.textContent = 'Simon Game';
+  info.classList.add('hidden');
+  tileContainer.classList.add('unclickable');
+  // if (gameLevel > GET HIGH SCORE)
+  //      New div that says new high score
+  // set high score send back to form
+  if (gameLevel > highScore) {
+    sethighScore();
   }
+}
 
-  function score() {
-    document.getElementById("score").innerHTML = gameLevel;
-    console.log(gameLevel);
-    }
-    
-    function sethighScore() {
-        // new html content
-        highScore.classList.remove('hidden');
-        }
+function score() {
+  document.getElementById("score").innerHTML = gameLevel;
+  console.log(gameLevel);
+}
+
+function sethighScore() {
+  // new html content
+  highScore.classList.remove('hidden');
+}
+
 startButton.addEventListener('click', startGame);
